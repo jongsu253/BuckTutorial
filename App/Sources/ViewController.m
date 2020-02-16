e0e2426a2a76f0bfa5a23b9e12f86347c14760fa
jongsu253 <jongsu253@gmail.com>
2020-02-17 03:28:11 +0900

    Hello Buck!

--- App/Sources/ViewController.m
+++ App/Sources/ViewController.m
@@ -1,10 +1,26 @@
 #import "ViewController.h"
 
 @implementation ViewController {
+  UILabel *_helloLabel;
 }
 
 - (void)viewDidLoad {
   self.view.backgroundColor = [UIColor whiteColor];
+  [self setupView];
+  [self setupLayout];
+}
+
+- (void)setupView {
+  _helloLabel = [[UILabel alloc] init];
+  _helloLabel.translatesAutoresizingMaskIntoConstraints = NO;
+  [_helloLabel setText:@"Hello Buck!"];
+  [self.view addSubview:_helloLabel];
+}
+
+- (void)setupLayout {
+  [_helloLabel.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+  [_helloLabel.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+  [_helloLabel.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
 }
 
 @end
